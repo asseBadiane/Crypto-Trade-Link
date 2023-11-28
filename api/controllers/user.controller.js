505bc2f9c388943ffb8c7b306ddc30b7b7dc1746
asseBadiane <badianeasse99@gmail.com>
2023-11-28 13:02:33 +0000

    Create DeleteUser functionality api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -31,3 +31,14 @@ export const updateUser = async (req, res, next )  => {
       next(error)
   }
 }
+
+export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id ) return next(errorHandler(401,"You can update only your own account!"));
+    try {
+       await User.findByIdAndDelete(req.params.id)
+       res.clearCookie('access_token') // delete cookie
+      res.status(200).json("User deleted successfully!" )
+    } catch (error) {
+        next(error)
+    }
+}
