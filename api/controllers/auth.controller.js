c732f1d5aa35088ae26c67e61c042685afdee845
asseBadiane <badianeasse99@gmail.com>
2023-12-05 15:17:21 +0000

    Change name file model

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/User.model.js";
 import bcrypt from "bcrypt";
 import Jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
